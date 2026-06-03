2695093d6888f677d374f18c2583d9f8c1134ef7
NicholasNPham <npham@sao10.com>
2026-06-03 16:14:04 -0400

    added imports, constants, and inputs

---
+++ main.py
@@ -0,0 +1,17 @@
+# STANDARD LIBRARY IMPORTS
+from collections import Counter
+import re
+from datetime import datetime
+
+# THIRD PARTY IMPORTS
+import win32com.client
+import openpyxl
+from openpyxl.styles import Font, PatternFill, Alignment
+
+# CONSTANTS
+SUBJECT = "[External] Message meets Alert condition"
+OUTPUT_FILE = "PARSED_INTRUSION_EMAILS.xlsx"
+
+# INPUTS
+DATE_START = datetime.strptime(input("Start date (YYYY-MM-DD): "), "%Y-%m-%d")
+DATE_END   = datetime.strptime(input("End date (YYYY-MM-DD): "), "%Y-%m-%d")
